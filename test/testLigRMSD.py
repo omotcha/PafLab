e71fc3f059a3df31b42c76f3fa8921e8a88c6965
omotcha <omotcha2097@gmail.com>
2022-10-27 11:43:24 +0800

    aug key file extractor

--- test/testLigRMSD.py
+++ test/testLigRMSD.py
@@ -6,6 +6,9 @@ tester of lig rmsd
 """
 
 import os
+
+import pandas as pd
+
 from utils.rmsd.ligrmsd import RMSDCalculator
 from configs.config import *
 
@@ -14,30 +17,13 @@ def test():
     ref_base = os.path.join(tmp_dir, "CASF_ledock_redocking_pdb2sdf")
     rmsd = []
     rmsd_helper = RMSDCalculator()
-    err_list = []
-    for pid in os.listdir(ref_base):
+    pids = os.listdir(ref_base)
+    for pid in pids:
         ligand = os.path.join(ref_base, pid, "{}_dock.sdf".format(pid))
         ligand_ref = os.path.join(coreset_dir, pid, "{}_ligand.sdf".format(pid))
-        item = None
-        try:
-            item = {pid: rmsd_helper.calc_rmsd(ligand, ligand_ref, "symmetric", minimize=False)}
-        except Exception as e:
-            # err_list.append(pid)
-            try:
-                ligand_ref = os.path.join(coreset_dir, pid, "{}_ligand.mol2".format(pid))
-                item = {pid: rmsd_helper.calc_rmsd(ligand, ligand_ref, "symmetric", minimize=False)}
-            except Exception:
-                # err_list.append(pid)
-                try:
-                    ligand_ref = os.path.join("E:\\datasets\\casf2016\\CASF-2016\\coreset_moe", "{}_ligand_moe.sdf".format(pid))
-                    item = {pid: rmsd_helper.calc_rmsd(ligand, ligand_ref, "symmetric", minimize=False)}
-                except Exception:
-                    err_list.append(pid)
-        if item is not None:
-            rmsd.append(item)
-    print(rmsd)
-    print(len(rmsd))
-    print(err_list)
+        rmsd.append(rmsd_helper.calc_rmsd(ligand, ligand_ref, "symmetric", minimize=False)[0])
+    ret = pd.DataFrame({"id": pids, "rmsd": rmsd})
+    ret.to_csv("./output/rmsd.csv", index=False)
 
 
 if __name__ == '__main__':
